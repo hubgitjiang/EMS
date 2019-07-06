094d5ea949b7ebfa8cc03297312e570f051c12c2
1122 <913009030@qq.com>
2019-07-06 17:08:22 +0800

    EMS项目

---
+++ src/kits/common.js
@@ -0,0 +1,6 @@
+﻿export default {
+	//dataApiDomain:'http://157.122.54.189:9093'  //数据api域名
+	dataApiDomain:'http://157.122.54.189:9093',  //数据api域名
+	// dataApiDomain:'http://vueapi.ittun.com',  //数据api域名
+	imgDomain:'http://ofv795nmp.bkt.clouddn.com/' //图片云服务器地址
+}
